3d5e3bd3fa4eaad97b7adb5b67c0da4796f1aa3c
garvit <garvitmaloo.21@gmail.com>
2023-10-21 22:21:00 +0530

    Fixing unit test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("App component is renders correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Not starting the app/i);
+  const linkElement = screen.getByText(/Starting the app/i);
   expect(linkElement).toBeInTheDocument();
 });
