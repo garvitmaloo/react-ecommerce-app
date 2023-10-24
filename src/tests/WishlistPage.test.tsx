27a5ce4b1aa844a71477aaaa750f462f28e05b7a
garvit <garvitmaloo.21@gmail.com>
2023-10-24 13:42:00 +0530

    Adding wishlist page UI

---
+++ src/tests/WishlistPage.test.tsx
@@ -0,0 +1,32 @@
+import { screen } from "@testing-library/react";
+import { rest } from "msw";
+
+import { renderWithProviders } from "../utils/utils-for-tests";
+import { worker } from "../mocks/browser";
+import WishlistPage from "../pages/WishlistPage";
+
+test("Error message is shown if HTTP request fails", async () => {
+  worker.resetHandlers(
+    rest.get(
+      "https://react-ecommerce-app-d25b3-default-rtdb.firebaseio.com/wishlist.json",
+      (req, res, ctx) => {
+        return res(ctx.status(404));
+      }
+    )
+  );
+
+  renderWithProviders(<WishlistPage />);
+
+  const errorMsg = await screen.findByTestId("error-msg");
+  expect(errorMsg).toBeInTheDocument();
+});
+
+test("Product card is rendered for every product in the wishlist", async () => {
+  renderWithProviders(<WishlistPage />);
+
+  const productCards = await screen.findAllByTestId("wishlist-product-card");
+  expect(productCards).toHaveLength(1); // length should be one because we are using Array.slice(1) in Wishlist page
+
+  const productCard = await screen.findByTestId("wishlist-product-card");
+  expect(productCard).toBeInTheDocument();
+});
